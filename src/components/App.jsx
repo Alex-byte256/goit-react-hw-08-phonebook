b7a63e72f182386bc106da7731773e69b5ceb965
Alex Kozak <56194044+Alex-byte256@users.noreply.github.com>
2023-04-03 23:19:03 +0300

    add fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,18 +24,18 @@ function  App (){
         <Routes>
           <Route path="/goit-react-hw-08-phonebook" element={<Layout/>}>
                 <Route index element={<HomePage/>} />
-                <Route path="contacts" element={<PrivateRoute
+                <Route path="/contacts" element={<PrivateRoute
                   redirectTo="login"
                   component={<Contacts />}
                 />} />
-                <Route path="register" element={<PublicRoute
+                <Route path="/register" element={<PublicRoute
                   restricted
-                  redirectTo="contacts"
+                  redirectTo="/contacts"
                   component={<Registration />}
                 />} />
-                <Route path="login" element={<PublicRoute
+                <Route path="/login" element={<PublicRoute
                   restricted
-                  redirectTo="contacts"
+                  redirectTo="/contacts"
                   component={<Login />}
                 />} />
           </Route>
